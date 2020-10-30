5129d19f8b78926bc67443782ce68459ced8ebf6
Justin Benz <jbenz156@gmail.com>
2020-10-30 13:36:26 -0400

    finished header and tabs files

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,24 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header = document.createElement('header')
+    const headerDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const headerTemp = document.createElement('span')
+
+    headerDate.textContent = "MARCH 28, 2020"
+    headerTitle.textContent = "Lambda Times";
+    headerTemp.textContent ="98°"
+
+    header.classList.add('header');
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp');
+
+    header.appendChild(headerDate);
+    header.appendChild(headerTitle);
+    header.appendChild(headerTemp);
+    return header;
+}
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,29 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+import axios from "axios"
+
+
+
+axios
+    .get("https://lambda-times-api.herokuapp.com/topics")
+    .then((res) => {
+        tabMaker(res.data);
+    })
+    .catch((fuzz) => {
+        console.log(fuzz)
+    })
+
+
+// 
+
+function tabMaker({ topics }) {
+    topics.forEach((item) => {
+        const topicsContainer = document.querySelector('.topics');
+        const tab = document.createElement('div');
+        tab.classList.add('tab');
+        tab.textContent = item;
+        topicsContainer.appendChild(tab);
+    })
+ 
+}
